db154749ac296c38c2b8d17c72e0dbd7bc33ac7d
Akhyar <77602343+akhyarsalleh@users.noreply.github.com>
2026-08-21 00:00:33 +0800

    Update checker.js

--- checker.js
+++ checker.js
@@ -497,7 +497,7 @@ window.runLicenseChecker = function(callback, daysThreshold) {
     if (expiredDisplayList.length > 0) {
         // Red critical state
         //var detailsHTML = "<b>[ EXPIRED CREDENTIALS ]</b><br>" + expiredDisplayList.join('');
-        var detailsHTML = "<span style='font-weight: bold; color: white; background: black;'> EXPIRED CREDENTIALS </span><br>" + expiredDisplayList.join('');
+        var detailsHTML = "<span style='font-weight: bold; color: white; background: #2c3e50; padding: 2px 5px;'> EXPIRED CREDENTIALS </span><br>" + expiredDisplayList.join('');
         if (expiringSoonDisplayList.length > 0) {
             detailsHTML += "<br><b>[ EXPIRING SOON ]</b><br>" + expiringSoonDisplayList.join('');
         }
