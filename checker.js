955bb0113dfe3375bb942ad6619660330be1b6ec
Akhyar <77602343+akhyarsalleh@users.noreply.github.com>
2026-08-21 00:53:19 +0800

    Update checker.js

--- checker.js
+++ checker.js
@@ -499,9 +499,9 @@ window.runLicenseChecker = function(callback, daysThreshold) {
     if (expiredDisplayList.length > 0) {
         // Red critical state
         //var detailsHTML = "<b>[ EXPIRED CREDENTIALS ]</b><br>" + expiredDisplayList.join('');
-        var detailsHTML = "<span style='text-decoration: underline; text-underline-offset: 6px; display: inline-block; margin-bottom: 10px;'><b> EXPIRED CREDENTIALS </b></span><br>" + expiredDisplayList.join('\n');
+        var detailsHTML = "<span style='text-decoration: underline; text-underline-offset: 6px; display: inline-block; margin-bottom: 10px;'><b> EXPIRED VALIDITY </b></span><br>" + expiredDisplayList.join('');
         if (expiringSoonDisplayList.length > 0) {
-            detailsHTML += "<br><span style='text-decoration: underline; text-underline-offset: 6px; display: inline-block; margin-bottom: 10px;'><b> EXPIRING SOON </b></span><br>" + expiringSoonDisplayList.join('\n');
+            detailsHTML += "<br><span style='text-decoration: underline; text-underline-offset: 6px; display: inline-block; margin-bottom: 10px;'><b> EXPIRING SOON </b></span><br>" + expiringSoonDisplayList.join('');
         }
         statusConfig = {
             overlayBg: 'rgba(235, 50, 35, 0.35)',
@@ -523,7 +523,7 @@ window.runLicenseChecker = function(callback, daysThreshold) {
             titleText: 'Qualification Expiring Soon',
             tagBg: '#f97316',
             tagText: 'FLY WITH CAUTION!',
-            detailsText: "<span style='text-decoration: underline; text-underline-offset: 6px; display: inline-block; margin-bottom: 10px;'><b> EXPIRING SOON </b></span><br>" + expiringSoonDisplayList.join('\n')
+            detailsText: "<span style='text-decoration: underline; text-underline-offset: 6px; display: inline-block; margin-bottom: 10px;'><b> EXPIRING SOON </b></span><br>" + expiringSoonDisplayList.join('')
         };
     } else {
         // Green valid state
